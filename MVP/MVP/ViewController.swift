f0dff85c842d10cf279d241afc437ebab4531b3b
Longo97 <marco.longobardi997@gmail.com>
2023-10-10 08:16:55 +0200

    MVP Project creation

---
+++ MVP/MVP/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by Marco Longobardi on 09/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
